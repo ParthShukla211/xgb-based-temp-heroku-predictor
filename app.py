36e300cf8430931a4d71fc53e689e690349061aa
Parth Shukla <shuklaparth501@gmail.com>
2022-07-03 18:47:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+
+pipe = pickle.load(open('xgbmodel.pkl','rb'))
+st.title('Physics Based Temp Predictor')
+
+
+LatDegree = st.number_input('LatDegree')
+LongDegree = st.number_input('LongDegree')
+MeasureDepth_m = st.number_input('MeasureDepth_m')
+SurfTemp = st.number_input('SurfTemp')
+
+
+
+# 'pH(CaCl2)':[pH(CaCl2)],'pH(H2O)':[pH(H2O)],
+if st.button('Predict Clay'):
+      input=pd.DataFrame({'LatDegree':[LatDegree],'LongDegree':[LongDegree],'MeasureDepth_m':[MeasureDepth_m],'SurfTemp':[SurfTemp]})
+      result = pipe.predict(input)
+      st.success('THE Clay FOR GIVEN DATA WILL BE {}'.format(result))
+# st.header(result)
